dfd3404286c5b0dbe32ac7071f68b1e37f23e443
Edward Chan <wado03@hotmail.com>
2011-04-12 02:36:29 -0700

    added request and response

--- app.js
+++ app.js
@@ -5,39 +5,24 @@ var app = metro.createServer();
 
 app.subscribe('/testexchange/test.route/tesetqueue', 'workflow1', function(workflow) {
 
-	workflow.http('stepone', function(http, data) {
+	workflow.http('stepone', function(req, res) {
 		console.log('stepone invoked');
-		app.publish('workflow1.steptwo', 'test');
+		res.publish('workflow1.steptwo', 'test');
 	});
 	
-	workflow.log('steptwo', function(logger, data) {
+	workflow.log('steptwo', function(req, res) {
 		console.log('steptwo invoked');
-		app.publish('workflow1.stepthree', 'test');
+		res.publish('workflow1.stepthree', 'test');
 	});
 	
-	workflow.http('stepthree', function(http, data) {
+	workflow.http('stepthree', function(req, res) {
 		console.log('stepthree invoked');
+		res.publish('workflow1.stepfour', 'test');
 	});
 	
-	workflow.code('stepfour', function(data) {
+	workflow.code('stepfour', function(req, res) {
 		console.log('stepfour invoked');
 	});
-});
-
-app.subscribe('/testexchange/test.route/tesetqueue2', 'workflow2', function(workflow) {
-
-	workflow.http('stepone', function(http, data) {
-		console.log('stepone invoked');
-	});
-	
-	workflow.log('steptwo', function(logger, data) {
-		console.log('steptwo invoked');
-	});
-	
-	workflow.http('stepthree', function(http, data) {
-		console.log('stepthree invoked');
-	});
-
 });
 
 app.listen();
