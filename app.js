9c18a736a7e119a534dcbe97f74b0bece8a6ad3c
Edward Chan <wado03@hotmail.com>
2011-04-12 08:52:19 -0700

    added workflow subscriptions

--- app.js
+++ app.js
@@ -2,26 +2,29 @@ var metro = require('./lib/metro');
 
 var app = metro.createServer();
 
-
-app.subscribe('/testexchange/test.route/tesetqueue', 'workflow1', function(workflow) {
+app.subscribe('testexchange/test.route/tesetqueue', 'workflow1', function(workflow) {
 
 	workflow.http('stepone', function(req, res) {
 		console.log('stepone invoked');
-		res.publish('workflow1.steptwo', 'test');
+		console.log(req.body);
+		res.next('steptwo');
 	});
 	
 	workflow.log('steptwo', function(req, res) {
 		console.log('steptwo invoked');
-		res.publish('workflow1.stepthree', 'test');
+		console.log(req.body);
+		res.next('stepthree', 'test');
 	});
 	
 	workflow.http('stepthree', function(req, res) {
 		console.log('stepthree invoked');
-		res.publish('workflow1.stepfour', 'test');
+		console.log(req.body);
+		res.next('stepfour', 'test');
 	});
 	
 	workflow.code('stepfour', function(req, res) {
 		console.log('stepfour invoked');
+		console.log(req.body);
 	});
 });
 
