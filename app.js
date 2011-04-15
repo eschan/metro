d03c93797c6a379906e9858cdd0f87f223f6b6bc
Edward Chan <wado03@hotmail.com>
2011-04-14 20:08:26 -0700

    added pass through data between steps

--- app.js
+++ app.js
@@ -7,18 +7,21 @@ app.subscribe('testexchange/test.route/tesetqueue', 'workflow1', function(workfl
 	workflow.http('stepone', function(req, res) {
 		console.log('stepone invoked');
 		console.log(req.body);
+		res.write('responseone', 'oh yeah!');
 		res.next('steptwo');
 	});
 	
 	workflow.log('steptwo', function(req, res) {
 		console.log('steptwo invoked');
 		console.log(req.body);
+		res.write('responsetwo', 'oh yes!');
 		res.next('stepthree', 'test');
 	});
 	
 	workflow.http('stepthree', function(req, res) {
 		console.log('stepthree invoked');
 		console.log(req.body);
+		res.write('responsethree', 'oh man!');
 		res.next('stepfour', 'test');
 	});
 	
